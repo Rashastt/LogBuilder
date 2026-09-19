f6e32850d81d8d5a105cc16091b4b51f7835ddf1
Rashast <270198283+Rashastt@users.noreply.github.com>
2026-09-19 12:08:45 -0300

    Renamed project to LogBuilder and fixed default settings

--- logcatcher/__init__.py
+++
@@ -1,6 +0,0 @@
-from .core import setup, save, log, clear, clear_memory, clear_logfile, dtn, current_conf
-import datetime
-
-def config():
-    raise AttributeError(f'{datetime.datetime.now} LogCatcher: "config" is not callable. Did you mean "logcatcher.setup"?'
-)
